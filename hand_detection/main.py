686b12b25348bb45ea035b580aa70e8e2c53d429
octavio-sosa <octavio@ku.edu>
2020-10-31 23:34:34 -0500

    draw hand-centroid and finger-tip in main()

--- hand_detection/main.py
+++ hand_detection/main.py
@@ -4,21 +4,25 @@ import process.hand as hp
 
 def main():
     capture = cv2.VideoCapture(0)
+    isHandHist = False
 
     while capture.isOpened():
         ret, frame = capture.read()
-        pressed_key = cv2.waitKey(1) 
+        pressed_key = cv2.waitKey(1)
 
-        if pressed_key & 0xFF == ord('q'): #quit
+        if pressed_key & 0xFF == ord('q'):
             break
-        elif pressed_key & 0xFF == ord('s'): #scan hand
-            frame = hp.draw_rect(frame)
-            frame = cv2.flip(frame, 1)
-            cv2.imshow('Hand-scan', frame)
+        elif pressed_key & 0xFF == ord('s'):
+            rectangles = hp.get_rectangles(frame)
+            handHist = hp.get_handHist(frame, rectangles)
+            isHandHist = True
+        elif isHandHist:
+            frame = hp.drawPOI(frame, handHist)
         else:
             frame = hp.draw_rect(frame)
             frame = cv2.flip(frame, 1)
-            cv2.imshow('Rectangles-screen', frame)
+
+        cv2.imshow('Live', frame)
 
     capture.release()
     cv2.destroyAllWindows()
