21a1176d25b847e1fda077b1d981b1697c698209
Hunter Glass <h_glass@ku.edu>
2020-11-04 21:12:46 -0600

    add test 2

--- scripts/test.js
+++ scripts/test.js
@@ -12,19 +12,27 @@ class testSuite {
 
     test1()
     {
-        console.log("Test1: Is Player initialized correctly?")
+        console.log("Test1: Is Player initialized correctly? Player should have 3 lives.")
         if(playerStatus.currentLives != 0)
         {
-            console.log("\nPlayer has ", playerStatus.currentLives, "lives, player is correctly initialized.\n")
+            console.log("Player has ", playerStatus.currentLives, "lives, player is correctly initialized.\n")
         }
         else
         {
-            console.log("\nPlayer has not been initialized correctly!\n");
+            console.log("Player has not been initialized correctly!\n");
         }
     }
     test2()
     {
-        console.log("Test2: ")
+        console.log("Test2: Is score initialized properly? Player should start with 0 score.")
+        if(playerStatus.currentScore == 0)
+        {
+            console.log("Player has ", playerStatus.currentScore, "lives, player score is correctly initialized.\n")
+        }
+        else
+        {
+            console.log("Player score is not correctly initialized!\n");
+        }
     }
     runTests()
     {
