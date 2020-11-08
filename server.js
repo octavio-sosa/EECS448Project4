ae0df6536f757947987f4f2ea5d441e711ca899c
octavio-sosa <octavio@ku.edu>
2020-11-07 18:14:28 -0600

    init nodejs setup

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const path = require('path')
+const { spawn } = require('child_process')
+
+const app = express();
+app.use('/scripts', express.static(path.join(__dirname, 'scripts')))
+app.use('/assets', express.static(path.join(__dirname, 'assets')))
+app.use('', express.static(path.join(__dirname)))
+
+app.get('/', (req, res)=>{
+	res.sendFile(path.join(__dirname,'index.html'))
+})
+
+app.get('/handDataStream', (req, res)=>{
+	const childPy = spawn('python3', ['static/py/hand_detection/handDataStreamer.py'])
+})
+
+app.get('/handDataRead', (req, res)=>{
+	const childPython = spawn('python3', ['static/py/hand_detection/handDataReader.py'])
+
+	childPython.stdout.on('data', (data)=>{
+		handData = data.toString()
+		res.json(handData)
+	})
+
+	childPython.stderr.on('data', (data)=>{
+		console.error('stderr: ',data.toString())
+	})
+
+	childPython.on('close', (code) => {
+		console.log('child process exited with code: ', code.toString()) 
+	})
+})
+
+app.listen(8000)
